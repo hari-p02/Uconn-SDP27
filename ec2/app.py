4f57ea043cc77d02a5fcf74f5af602fdba525860
Hari Patchigolla <haripat@Haris-MacBook-Pro.local>
2024-03-20 03:57:32 -0400

    changed app.py to decode files for adding

--- ec2/app.py
+++ ec2/app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/numbers', methods=['POST'])
 def handle_post():
-    json = request.json
-    contents = base64.b64decode(json.loads(event['body'])['file-contents'])
+    try:
+        json = request.json
+        for key in json.keys():
+            contents = base64.b64decode(json[key])
+            with open(f'/home/ec2-user/data/{key}.txt', 'wb') as file:
+                file.write(contents)
+        return jsonify({}), 200
+    except:
+        return jsonify({}), 400
+
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
