d464a35fa01e2bb3d04297f7b459a17dc4396a20
Hari Patchigolla <haripat@Haris-MacBook-Pro.local>
2024-03-20 03:48:42 -0400

    changed code to ee searlizew

--- localapp.py
+++ localapp.py
@@ -16,4 +16,16 @@ def read_file_into_base64_string(file_path):
     bin = f.read()
     return (base64.b64encode(bin)).decode('ascii')
 
+def serilize(x, y):
+    args = [x, y]
+    command = [executable_path] + args
+    result = subprocess.run(command, capture_output=True, text=True, cwd=folder_path)
+    if result.returncode == 0:
+        print("Execution successful.")
+        print("Output:", result.stdout)
+    else:
+        print("Execution failed.")
+        print("Error:", result.stderr)
+    
+    
 
